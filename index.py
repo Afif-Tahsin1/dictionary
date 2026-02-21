d4bbb39e0558844695d67081fff539fe0acc06c7
hhh <jareen0512@gmail.com>
2026-02-21 19:47:12 +0600

    1

---
+++ index.py
@@ -0,0 +1,18 @@
+student_data = {
+    "id1" : {"name" : "Sara", "class" : "V", "subject_interagtion" : "english, math, science"},
+    "id2" : {"name" : "David", "class" : "V", "subject_interagtion" : "english, math, science"},
+    "id3" : {"name" : "Sara", "class" : "V", "subject_interagtion" : "english, math, science"},
+    "id4" : {"name" : "Surya", "class" : "V", "subject_interagtion" : "english, math, science"},
+}
+
+result = {}
+seen = set()
+
+for student_id, details in student_data.items():
+    unique_key = (details["name"], details["class"],details["subject_interagtion"])
+    if unique_key not in seen:
+        seen.add(unique_key)
+        result[student_id] = details
+
+for k, v in result.items():
+    print(k, ":", v)
